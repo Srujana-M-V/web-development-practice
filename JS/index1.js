04c5929f5e3ad901399d3aba4f69c72b720b014d
Srujanasgr <srujanamv123@gmail.com>
2026-05-14 16:18:05 +0530

    higher order functions

---
+++ JS/index1.js
@@ -0,0 +1,12 @@
+document.querySelector("title").innerHTML="srujana";
+let heading = document.querySelector("h1");
+heading.innerHTML = "Srujana";
+heading.style.color = "blue";
+document.querySelectorAll("li")[2].innerHTML="Learning DOM"
+document.querySelector("li a").style.color="red";
+document.querySelector(".btn")
+document.querySelector(".btn").addEventListener("click", function() {
+});
+document.querySelector(".btn").addEventListener("click", function() {
+    document.querySelector("h1").innerHTML = "Button Clicked";
+});
