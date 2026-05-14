4874b4896b6ee8f0a066aacd9b312edac50fc771
Srujanasgr <srujanamv123@gmail.com>
2026-05-14 22:29:10 +0530

    added constructor function

--- JS/index2.js
+++ JS/index2.js
@@ -17,3 +17,11 @@ return num1 / num2;
 function calculator(num1, num2, operator) {
 return operator(num1, num2);
 }
+function bellBoy(name,age,clas,place){
+    this.name=name;
+    this.age=age;
+    this.class=clas;
+    this.place=place;
+}
+var bellBoy1=new bellBoy("srujan",20,6,"sagar");
+var bellBoy2=new bellBoy("ashwin",6,1,"bangalore");
